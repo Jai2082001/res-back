c924b11bd91e83ccb4901c473896cb0b8425b0e3
jaideep grover <jaideepgrover147@gmail.com>
2024-11-07 19:54:35 -0500

    a

--- passport/passport.js
+++ passport/passport.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'https://cap-back.jaideepgrover.blog.com/auth/google/callback',
+      callbackURL: 'https://mmmm/auth/google/callback',
       scope: ['profile', 'email'],
     },
     async (accessToken, refreshToken, profile, done) => {
