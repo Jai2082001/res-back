de69742c88bbc3c8711697cfc6c187b291f98a93
jaideep grover <jaideepgrover147@gmail.com>
2024-11-22 17:55:50 -0500

    commit

--- index.js
+++ index.js
@@ -28,8 +28,6 @@ app.use(cors({
 
 app.use(passport.initialize())
 
-app.enable('trust proxy')
-
 app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
 
 app.get(
@@ -61,6 +59,7 @@ app.get(
 
         res.cookie('token', token, {
             httpOnly: true,  // Prevents JavaScript access to the cookie
+            secure: process.env.NODE_ENV === 'production',  // Only send in production over HTTPS
             sameSite: 'Strict',  // Limits cookie to your site only
             maxAge: 24 * 60 * 60 * 1000  // 1 day
         });
