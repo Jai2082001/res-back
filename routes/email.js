47564a58f416ad77d8b2a2102b961039c97f0956
jaideep grover <jaideepgrover147@gmail.com>
2024-11-23 09:46:54 -0500

    a

--- routes/email.js
+++ routes/email.js
@@ -73,25 +73,24 @@ router.post('/api/tweet', (req, res, next) => {
   
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("Authorization", "OAuth oauth_consumer_key=\"Tw4L0YpkIg3Sxv5hIgsIsMXsf\",oauth_token=\"1772748771404685312-0kveP1p5U8Txt6YYQKpmVkoDO9zHDz\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1732227998\",oauth_nonce=\"sdXWSuu74yX\",oauth_version=\"1.0\",oauth_signature=\"sVVpq78btg2VP1RTxEpqCCt5v2M%3D\"");
-  // console.log(text)
+  myHeaders.append("Authorization", "OAuth oauth_consumer_key=\"Tw4L0YpkIg3Sxv5hIgsIsMXsf\",oauth_token=\"1772748771404685312-0kveP1p5U8Txt6YYQKpmVkoDO9zHDz\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1732373052\",oauth_nonce=\"urvL3vcm2kR\",oauth_version=\"1.0\",oauth_signature=\"LHQSqJRiQKpCToIiWrP5wmRDsmI%3D\"");
+  myHeaders.append("Cookie", "guest_id=v1%3A173237299086891405; guest_id_ads=v1%3A173237299086891405; guest_id_marketing=v1%3A173237299086891405; personalization_id=\"v1_qzg6IAbcDHcKd8ScgHUlmg==\"");
+  
   const raw = JSON.stringify({
-    "text": req.body.tweetText
+    "text": req.body.twitterText
   });
-
+  
   const requestOptions = {
     method: "POST",
     headers: myHeaders,
-    body:raw,
+    body: raw,
     redirect: "follow"
   };
-
+  
   fetch("https://api.twitter.com/2/tweets", requestOptions)
     .then((response) => response.text())
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((error) => {res.send(result)});
+    .then((result) => res.send(result))
+    .catch((error) => res.send(error));
 
 })
 
@@ -118,3 +117,4 @@ router.get('/email/send', (req, res, next) => {
 
 
 module.exports = router
+
